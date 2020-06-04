655472ce59f7c7b29986f723ce0956b89a02cc0b
Kim Dang <kimdang229@gmail.com>
2020-06-03 22:14:23 -0700

    finish bare minimum

--- datastore/counter.js
+++ datastore/counter.js
@@ -2,19 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const sprintf = require('sprintf-js').sprintf;
 
-var counter = 0;
+// var counter = 0;
 
 // Private helper functions ////////////////////////////////////////////////////
 
 // Zero padded numbers can only be represented as strings.
 // If you don't know what a zero-padded number is, read the
 // Wikipedia entry on Leading Zeros and check out some of code links:
-// https://www.google.com/search?q=what+is+a+zero+padded+number%3F
-
+////  https://www.google.com/search?q=what+is+a+zero+padded+number%3F
+//
 const zeroPaddedNumber = (num) => {
   return sprintf('%05d', num);
-};
-
+};//
+//
 const readCounter = (callback) => {
   fs.readFile(exports.counterFile, (err, fileData) => {
     if (err) {
@@ -38,13 +38,23 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
-};
-
-
-
+exports.getNextUniqueId = (callback) => {
+  var counter;
+  readCounter((err, fileData) => {
+    if (err) {
+      console.log('Not working');
+    } else {
+      fileData++;
+      writeCounter(fileData, (err, id) => {
+        if (err) {
+          callback(err)
+        } else {
+          callback(null, id);
+          }
+       });
+    };
+  });
+}
 // Configuration -- DO NOT MODIFY //////////////////////////////////////////////
 
 exports.counterFile = path.join(__dirname, 'counter.txt');
--- datastore/index.js
+++ datastore/index.js
@@ -2,16 +2,32 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
+const Promise = require('bluebird');
+// const readdir = Promise.promisify(fs.readdir);
+// const mapp = Promise.promisify(_.map);
+// const readOneFile = Promise.promisify(exports.readOne);
+
 
 var items = {};
 
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
-};
+  counter.getNextUniqueId((err, id) => {
+    if (err) {
+      throw err;
+    } else {
+      var newFile = path.join(exports.dataDir, id + '.txt');
+      fs.writeFile(newFile, text, (err) => {
+        if (err) {
+          console.log('error writing new file');
+        } else {
+          callback(null, { id, text })
+        }
+      });
+    }
+  });
+}
 
 exports.readAll = (callback) => {
   var data = _.map(items, (text, id) => {
@@ -20,35 +36,73 @@ exports.readAll = (callback) => {
   callback(null, data);
 };
 
+
+exports.readAll = (callback) => {
+  console.log(exports.readOne);
+  const readOneFile = Promise.promisify(exports.readOne);
+    fs.readdir(exports.dataDir, (err, data) => {
+      if (err) {new Error(`There are no files in the director`);}
+      else {
+        var file = _.map(data, (text) => {
+        text = text.slice(0, 5);
+        console.log(text);
+        var id = text;
+        return readOneFile(id);
+      })
+      Promise.all(file).then(value => {
+        callback(null, value);
+      })
+    } })
+
+
+};
+
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  var currentFile = path.join(exports.dataDir, id + '.txt');
+  fs.readFile(currentFile, (err, data) => {
+    if (err || data === undefined) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      let text = data.toString();
+      callback(null, { id, text });
+    }
+  })
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  let filePath = path.join(exports.dataDir, id + '.txt');
+  exports.readOne(id, (err, data) => {
+    if (err || data === undefined) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.writeFile(filePath, text, (err) => {
+        if (err) {
+          callback(new Error(`No item with id: ${id}`));
+        } else {
+          callback(null, { id, text });
+        }
+      })
+    }
+  })
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
-};
+  let filePath = path.join(exports.dataDir, id + '.txt');
+  exports.readOne(id, (err) => {
+      if (err) {
+        // report an error if item not found
+        callback(new Error(`No item with id: ${id}`));
+      } else {
+        fs.unlink(filePath, (err) => {
+          if (err) {
+            callback(new Error(`No item with id: ${id}`));
+          } else {
+            callback(null);
+          }
+      })
+    }
+  })
+}
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
 
